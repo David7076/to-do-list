dfd7782d85450c7e40379c30e632de11ab1b87af
David7076 <david.santos.lima7076@gmail.com>
2026-08-26 22:04:41 -0300

    feat- adicionando AppNavigation, screen do formulario e lista de de tarefas screen

---
+++ app/src/main/java/com/david7076/to_do_list/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.david7076.to_do_list
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.david7076.to_do_list.navigation.AppNavigation
+import com.david7076.to_do_list.ui.theme.TodolistTheme
+import com.david7076.to_do_list.viewmodel.TarefaViewModel
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TodolistTheme {
+                val viewModel: TarefaViewModel = viewModel(
+                    factory = TarefaViewModel.factory(applicationContext)
+                )
+                AppNavigation(viewModel = viewModel)
+            }
+        }
+    }
+}
